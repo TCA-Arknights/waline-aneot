692314ba15ad7be18ebc61b7f743881b16f79e65
泰拉创作者联合会 <TCA_doc@163.com>
2024-12-08 14:59:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
+const GPTReviewer = require('waline-plugin-llm-reviewer');
 
-module.exports = Application({
-  plugins: [],
-  async postSave(comment) {
-    // do what ever you want after comment saved
-  },
+module.exports = Waline({
+  plugins: [
+    GPTReviewer({
+        openaiBaseUrl: process.env.OPENAI_BASE_URL,
+        openaiModel: process.env.OPENAI_MODEL,
+        openaiApiKey: process.env.OPENAI_API_KEY,
+        openaiPrompt: process.env.OPENAI_PROMPT,
+    })
+  ]
 });
