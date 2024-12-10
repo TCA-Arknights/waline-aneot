ae6fff75f08d5625f13ed62d0ca0284676473b3b
泰拉创作者联合会 <TCA_doc@163.com>
2024-12-09 19:43:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Waline = require('@waline/vercel');
-const GPTReviewer = require('waline-plugin-llm-reviewer');
+const LLMReviewer = require('waline-plugin-llm-reviewer');
 
 module.exports = Waline({
   plugins: [
-    GPTReviewer({
-        openaiBaseUrl: process.env.OPENAI_BASE_URL,
-        openaiModel: process.env.OPENAI_MODEL,
-        openaiApiKey: process.env.OPENAI_API_KEY,
-        openaiPrompt: process.env.OPENAI_PROMPT,
+    LLMReviewer({
+        LLMApiUrl: process.env.LLM_API_URL,
+        LLMModel: process.env.LLM_MODEL,
+        LLMApiKey: process.env.LLM_API_KEY,
+        LLMPrompt: process.env.LLM_PROMPT,
+        LLMReason: process.env.LLM_REASON,
     })
   ]
 });
